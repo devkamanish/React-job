062bc7862cf952b63a36aab5b8e6bb273ab39b65
devkamanish <devkamanish61@gmail.com>
2024-06-12 10:21:34 +0530

    add : deploy workflow

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,7 +8,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? '/api/jobs?_limit=3' : 'https://react-job-g4os.vercel.app/api/jobs/';
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs/';
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base :"/React-job/",
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://react-job-g4os.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
